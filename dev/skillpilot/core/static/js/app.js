023eac0284739b5f4ca876e93e5b8e8697a7733a
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-04-05 17:32:05 +0100

    add boostrap client-side validation to student signup and login forms

--- dev/skillpilot/core/static/js/app.js
+++ dev/skillpilot/core/static/js/app.js
@@ -6,3 +6,24 @@ let message = document.querySelector('#flash-message')
 setTimeout(function() {
     message.style.display = "none";
 }, 5000);
+
+
+// bootstrap client-side form validation
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    }, false)
+    })
+})()
