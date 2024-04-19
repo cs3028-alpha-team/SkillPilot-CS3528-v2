760d5407b8a4777b6ae1ee6f2800b63c45a82498
ibrahimAlkali <ibraalkali@outlook.com>
2024-04-19 15:43:24 +0100

    testing code + fixed up some views to produce errors

--- dev/skillpilot/core/urls.py
+++ dev/skillpilot/core/urls.py
@@ -48,7 +48,6 @@ urlpatterns = [
     # path to delete a recruiter profile associated, and student application connected to it
     path('delete-recruiter', views.delete_recruiter, name='delete-recruiter'),
 
-
     # paths related to student authentication and authorization
     path('student-signup', views.student_signup, name='student-signup'),
     path('student-login', views.student_login, name='student-login'),
@@ -60,7 +59,7 @@ urlpatterns = [
     path('recruiter-signup', views.recruiter_signup, name='recruiter-signup'),
     path('recruiter-login', views.recruiter_login, name='recruiter-login'),
 
-    # logout route generalized to all users 
+    # logout route generalized to all users
     path('logout', views.user_logout, name='logout'),
 
     # paths available to the admin to query the database from the admin dashboard
@@ -72,8 +71,7 @@ urlpatterns = [
     path('student-details/<str:studentID>/', views.student_details, name='student-details'),
     path('recruiter-details/<str:recruiterID>/', views.recruiter_details, name='recruiter-details'),
     path('internship-details/<str:internshipID>/', views.internship_details, name='internship-details'),
-
-    
-    # updates the outcome of an interview to accepted/rejected
+  
+    # updates the outcome of an interview to accepted/rejected 
     path('update-interview/<interview_id>/', views.update_interview, name='update-interview'),
 ]
