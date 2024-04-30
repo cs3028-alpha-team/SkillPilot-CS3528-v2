a43d951acc71f4967ed2771c04c45977d7a9b8f5
shaun-macdonald <91413584+shaun-macdonald@users.noreply.github.com>
2024-04-30 18:48:37 +0100

    Update settings.py

--- dev/skillpilot/skillpilot/settings.py
+++ dev/skillpilot/skillpilot/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ghsloj9x4f@ct2&w7fi3$h53$si^2)jmzga_m8#%3nw(#g75ps
 DEBUG = True
 ALLOWED_HOSTS = ['skillpilotv2-bf0fdedf7855.herokuapp.com']
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
